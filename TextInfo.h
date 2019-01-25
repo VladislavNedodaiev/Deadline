943dff485729609ffbfb6d1b3cb6b89eddbb4122
Vladyslav Nedodaiev <vladislavnedodaiev@gmail.com>
2019-01-26 01:38:57 +0200

    +TextInfo struct realisation added; +Text class realisation added;

--- TextInfo.h
+++ TextInfo.h
@@ -1,5 +1,35 @@
 #pragma once
 
-struct TextInfo {
+#include <SFML\Graphics.hpp>
+
+struct TextInfo
+{
+
+	enum eHorizontalAlign
+	{
+		Left = 0,
+		Middle = 1,
+		Right = 2
+	};
+	enum eVerticalAlign
+	{
+		Top = 0,
+		Center = 1,
+		Bottom = 2
+	};
+
+	eHorizontalAlign horizontalAlign;
+	eVerticalAlign verticalAlign;
+
+	sf::Font *font;
+	unsigned int characterSize;
+	int lineSpacing;
+	sf::Vector2f margin;
+	sf::Color textColor;
+	sf::Color backgroundColor;
+
+	TextInfo();
+	TextInfo(const TextInfo &textinfo);
+	void operator =(const TextInfo &textinfo);
 
 };
