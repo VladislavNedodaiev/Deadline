fc87b220906da81dd3cac3ffec80994bdde15cac
Vladyslav Nedodaiev <vladislavnedodaiev@gmail.com>
2019-01-26 01:46:13 +0200

    +Menu class realisation added;
    // FIRST COMPILATION SUCCEEDED!

--- Menu.cpp
+++ Menu.cpp
@@ -1,1 +1,69 @@
 #include "Menu.h"
+
+Menu::Menu(GameHelper &gameHelper) : State(gameHelper)
+{
+
+	this->loadContent().initialize();
+
+} // constructor
+
+Menu::~Menu()
+{
+
+	this->unloadContent();
+
+} // destructor
+
+Menu& Menu::initialize()
+{
+
+	// TODO
+
+	return *this;
+
+} // initializing
+
+Menu& Menu::loadContent()
+{
+
+	// TODO
+
+	return *this;
+
+} // loading
+
+Menu& Menu::unloadContent()
+{
+
+	// TODO
+
+	return *this;
+
+} // unloading
+
+Menu& Menu::input()
+{
+
+	// TODO
+
+	return *this;
+
+} // input
+
+Menu& Menu::update(float dt)
+{
+
+	// TODO
+
+	return *this;
+
+} // updating
+
+Menu& Menu::render()
+{
+
+	// TODO
+
+	return *this;
+
+} // rendering
