b7ac99bbc9b0a0d9ffb5fea75df0a710b15e6655
Vladyslav Nedodaiev <vladislavnedodaiev@gmail.com>
2019-01-26 02:21:01 +0200

    +SpriteSheet class added; +Behaviour class added; +GameObject class updated; +GameObject class realisation added; +Animation class added; +Button class added; +CounterBar class added.

--- GameObject.h
+++ GameObject.h
@@ -1,5 +1,45 @@
 #pragma once
 
-class GameObject {
+#include <SFML\Graphics.hpp>
+
+#include "Animation.h"
+
+class GameObject : public sf::RectangleShape
+{
+protected:
+
+	sf::Texture _texture;
+
+	Animation *_currentAnimation;
+
+public:
+
+	enum eInputType
+	{
+		None = 0
+	};
+
+	bool isVisible;
+	bool isMovable;
+
+	GameObject();
+	GameObject(sf::Image &imageSheet);
+	GameObject(sf::Image &imageSheet, float fps, sf::IntRect firstFrame);
+	GameObject(sf::Image &imageSheet, int frames, float fps, sf::IntRect firstFrame);
+	virtual ~GameObject();
+
+	virtual GameObject& setAnimation(int animation);
+	virtual GameObject& setFrame(int frame);
+	virtual Animation* getAnimation();
+
+	virtual bool pointIsOver(sf::Vector2i &point) const;
+	virtual bool isPressed(sf::Event &event, sf::Vector2i &point) const;
+
+	virtual GameObject& move(const sf::Vector2f &offset);
+	virtual GameObject& move(float offsetX, float offsetY);
+
+	virtual int input(sf::Event &event, sf::Vector2i mousePos);
+	virtual GameObject& update(float dt);
+	virtual GameObject& render(sf::RenderTarget &target);
 
 };
